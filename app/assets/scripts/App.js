ee676d24ad555e845f767c9fb1e461cb9cc8c8c6
Christie <thornton.christie@gmail.com>
2017-11-25 21:38:45 -0500

    Online portfolio layout complete.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+
+import $ from 'jquery';
+
+alert('hi');
+//open clickable link in new window
+$('#travel-site-ql').click(function(){
+    alert('hi');
+    window.open('https://christhorn10.github.io/travel-site/', '_blank');
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -15,5 +15,25 @@ gulp.task('watch', function(){
 		browserSync.reload();
 		});
 	
+    //watch any future folders in styles, watch any .css files
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+		});
+    
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+        });
     
 	});
+
+
+//styles is a dependency to cssInject.  cssInject wont begin until styles has run.
+gulp.task('cssInject',['styles'] , function(){
+	return gulp.src('./app/temp/styles/styles.css')
+		.pipe(browserSync.stream());
+	});	
+
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+    browserSync.reload();
+    });
